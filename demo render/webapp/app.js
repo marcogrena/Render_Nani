0ace609eebcfaacc5f15fee7ae0a54acea39b9f0
marcogrena <33692969+marcogrena@users.noreply.github.com>
2025-01-18 09:16:14 +0100

    Update app.js
    aggiunta rotta get /API/

--- demo render/webapp/app.js
+++ demo render/webapp/app.js
@@ -12,6 +12,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/API/", (req, res) => {
+     const risposta={
+        "auth": process.env.AUTHOR,
+        "message": process.env.MESSAGE
+     };
+     res.send(JSON.stringify(risposta));   
+    }
+);
 // Avvia il server
 app.listen(process.env.PORT, () => {
     console.log(`Server raggiungibile all'indirizzo http://localhost:${process.env.PORT}`);
